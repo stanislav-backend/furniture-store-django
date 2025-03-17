a44a0b3662c89e19e9aa0c80aacf9470591642fc
Стас Тимошенко <eswenko11@gmail.com>
2025-03-17 21:15:50 +0300

    debug-toolbar added

--- app/urls.py
+++ app/urls.py
@@ -1,19 +1,5 @@
-"""
-URL configuration for app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from app.settings import DEBUG
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 from django.contrib import admin
 from django.urls import include, path
@@ -23,3 +9,6 @@ urlpatterns = [
     path("", include("main.urls", namespace="main")),
     path("catalog/", include("goods.urls", namespace="catalog")),
 ]
+
+if DEBUG:
+    urlpatterns += debug_toolbar_urls()
