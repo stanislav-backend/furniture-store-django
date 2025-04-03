559f46e778eb7fbe3a1dfb75380201910b242238
Стас Тимошенко <eswenko11@gmail.com>
2025-04-03 20:58:25 +0300

    Full text search

--- goods/utils.py
+++ goods/utils.py
@@ -10,7 +10,11 @@ def q_search(query):
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    return (
+        Products.objects.annotate(rank=SearchRank(vector, query))
+        .filter(rank__gt=0)
+        .order_by("-rank")
+    )
 
     # keywords = [word for word in query.split() if len(word) > 2]
 
