1e6e3a39a7d8ff656c2366e25903063b4c8a47c6
Стас Тимошенко <eswenko11@gmail.com>
2025-04-10 15:59:08 +0300

    All cart templates were included

--- app/settings.py
+++ app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "main",
     "goods",
     "users",
+    "carts",
 ]
 
 MIDDLEWARE = [
@@ -144,4 +145,4 @@ INTERNAL_IPS = [
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
-LOGIN_URL = "/user/login/"
+LOGIN_URL = "/user/login"
